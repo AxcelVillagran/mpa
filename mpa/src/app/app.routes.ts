40b43abf6c3707deaf733a10408a4fd884a93db4
Axcel Villagran <axvigarc@espol.edu.ec>
2023-12-04 10:48:20 -0500

    Datos agregados a vista

--- mpa/src/app/app.routes.ts
+++ mpa/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DatosComponent } from './pages/datos/datos.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'details', component:DatosComponent}
 ];
