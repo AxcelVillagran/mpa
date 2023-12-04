009db2d6b9493f98986687d4ca724fa6f4aedbee
EmilioSG23 <eisaenz@espol.edu.ec>
2023-12-04 10:55:01 -0500

    Muestra de datos

--- mpa/src/app/pages/index/index.component.ts
+++ mpa/src/app/pages/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DatosComponent } from '../datos/datos.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DatosComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
